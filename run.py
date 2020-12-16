f339971ace66bb1ed2a4dea96efd2e70b7ebd1e2
LaDane <us.aw@hotmail.com>
2020-12-17 00:31:40 +0100

    running bot

---
+++ run.py
@@ -0,0 +1,12 @@
+# must install OpenJDK 13.0.2 and Lavalink #1079
+# navigate to OpenJDK bin folder
+# java -jar Lavalink.jar
+
+from bot import DiscordLeif
+
+def main():
+    bot = DiscordLeif()
+    bot.run()
+
+if __name__ == "__main__":
+    main()
